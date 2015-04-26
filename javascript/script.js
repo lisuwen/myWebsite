e8c5266aa0479a9340cdc6483ddcf3b282ece4a1
lisuwen <li.suwen@hotmail.com>
2015-04-26 09:32:28 -0400

    spelling

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,2 @@
-document.getElementByID("foot01").innerHTML =
-"<p>&copy; "+ new Date().getFullYear()+ "Suwen Li. All rights reserved.</p>";
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Suwen Li. All rights reserved.</p>";
